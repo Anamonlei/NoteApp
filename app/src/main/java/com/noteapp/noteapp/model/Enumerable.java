5fba78e4135e81cb24282296f876da0492223237
Ana Montiel <anamonlei51@gmail.com>
2017-01-12 10:09:46 +0100

    Initial commit

---
+++ app/src/main/java/com/noteapp/noteapp/model/Enumerable.java
@@ -0,0 +1,11 @@
+package com.noteapp.noteapp.model;
+
+import java.util.List;
+
+public interface Enumerable<T> {
+    int count();
+    void add(T element);
+    void remove(int index);
+    void update(int index, Note newElement);
+    List<T> getAll();
+}
