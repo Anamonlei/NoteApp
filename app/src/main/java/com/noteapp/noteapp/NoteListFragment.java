7fec67a77e776290a9aa5507311289b1bfd74983
Ana Montiel <anamonlei51@gmail.com>
2017-01-19 13:43:57 +0100

    Added adapter

--- app/src/main/java/com/noteapp/noteapp/NoteListFragment.java
+++ app/src/main/java/com/noteapp/noteapp/NoteListFragment.java
@@ -3,27 +3,34 @@ package com.noteapp.noteapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class NoteListFragment extends Fragment {
 
+    private RecyclerView recyclerNotesView;
 
     public NoteListFragment() {
-        // Required empty public constructor
+
     }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_note_list, container, false);
+
+        recyclerNotesView = (RecyclerView) view.findViewById(R.id.fragment_note_list_note_list);
+        recyclerNotesView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        return view;
+
     }
 
+    public void setAdapter(NoteAdapter adapter) {
+        recyclerNotesView.setAdapter(adapter);
+    }
 }
